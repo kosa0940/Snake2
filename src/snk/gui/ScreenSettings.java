0319dac44830e9d7507e47bad0c63885966aa51c
Paweł Kośnica <pawel.kosnica@gmail.com>
2017-11-18 17:26:39 +0100

    test

--- src/snk/gui/ScreenSettings.java
+++ src/snk/gui/ScreenSettings.java
@@ -1,4 +1,26 @@
 package snk.gui;
 
+import java.awt.*;
+
 public class ScreenSettings {
+
+    //Graphics display objects
+    private static GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    private static GraphicsDevice device = env.getDefaultScreenDevice();
+
+    //ScreenSize
+    public static int screenWidth=(int)env.getMaximumWindowBounds().getWidth();
+    public static int screenHeight=(int) env.getMaximumWindowBounds().getHeight();
+
+    public static double ratio = (double)screenWidth/screenHeight;
+
+    //Display mode
+    public static DisplayMode displayMode = new DisplayMode(800,600,16,DisplayMode.REFRESH_RATE_UNKNOWN);
+
+    public static void main(String[] args){
+        System.out.println(screenHeight);
+        System.out.println(screenWidth);
+        System.out.println(ratio);
+    }
+
 }
---
+++ src/snk/gui/mainScreen/Main.java
@@ -0,0 +1,17 @@
+package snk.gui.mainScreen;
+
+import javax.swing.*;
+
+public class Main extends JFrame{
+
+    public Main(){
+
+        super("Snake Paweł Kośnica (unplayable)");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+
+    }
+
+}

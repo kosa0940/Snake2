82d0061033f5d5f4d15fbd91b76a5ce7c4bbfada
Paweł Kośnica <pawel.kosnica@gmail.com>
2017-11-19 12:02:48 +0100

    Creating a GameField Panel

--- src/snk/gui/ScreenSettings.java
+++ src/snk/gui/ScreenSettings.java
@@ -9,15 +9,8 @@ public class ScreenSettings {
     public static GraphicsDevice device = env.getDefaultScreenDevice();
 
     //ScreenSize
-    public static int screenWidth=(int)env.getMaximumWindowBounds().getWidth();
-    public static int screenHeight=(int) env.getMaximumWindowBounds().getHeight();
+    public static int screenWidth = (int) env.getMaximumWindowBounds().getWidth();
+    public static int screenHeight = (int) env.getMaximumWindowBounds().getHeight();
 
-    public static double ratio = (double)screenWidth/screenHeight;
-
-    public static void main(String[] args){
-
-        System.out.println(screenWidth);
-
-    }
-
+    public static double ratio = (double) screenWidth / screenHeight;
 }
---
+++ src/snk/gui/mainScreen/GameField.java
@@ -0,0 +1,19 @@
+package snk.gui.mainScreen;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameField extends JPanel{
+
+    //Panel look
+    private static Color backgroundColor = new Color(23, 156, 29);
+    private static int gameFieldWidth;
+    private static int gameFieldHeight;
+
+    public GameField(){
+
+        setForeground(backgroundColor);
+
+    }
+
+}
